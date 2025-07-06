51eec87ba740ce9b8118c70fce30a4a701bd56f5
Abdallah9oo <abdoneuer67@gmail.com>
2025-07-06 18:57:38 +0300

    Add project files.

---
+++ Demo.DAL/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.DAL.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string? ImageName { get; set; }
+        public string Phone { get; set; }
+        public bool IsActive { get; set; }
+        public Department? Department { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
---
+++ Demo.PL/Controllers/DepartmentsController.cs
@@ -0,0 +1,95 @@
+﻿namespace Demo.PL.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        //private IGenericRepository<Department> _repo;
+        private IDepartmentRepository _repo;
+
+        public DepartmentsController(IDepartmentRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var departments = _repo.GetAllAsync();
+            return View(departments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department department)
+        {
+            if (!ModelState.IsValid) return View(department);
+            _repo.CreateAsync(department);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id) => DepartmentControllerHandler(id, nameof(Details));
+
+        public IActionResult Edit(int? id) => DepartmentControllerHandler(id, nameof(Edit));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Department department)
+        {
+
+            if (id != department.Id) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _repo.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+
+            return View(department);
+        }
+
+        public IActionResult Delete(int? id) => DepartmentControllerHandler(id, nameof(Delete));
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            var department = await _repo.GetAsync(id.Value);
+            if (department is null) return NotFound();
+
+            try
+            {
+                _repo.Delete(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View(department);
+        }
+
+        private IActionResult DepartmentControllerHandler(int? id, string viewName)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            var department = _repo.GetAsync(id.Value);
+            if (department is null) return NotFound();
+
+            return View(viewName, department);
+        }
+    }
+}
